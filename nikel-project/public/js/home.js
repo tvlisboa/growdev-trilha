1f6322d9e26ece9494e4ed73f105a387130370ca
tvlisboa <tvlisboa@hotmail.com>
2025-11-23 19:09:08 -0300

    implementando o login e suas transacoes

---
+++ nikel-project/public/js/home.js
@@ -0,0 +1,59 @@
+const myModal = new bootstrap.Modal("#transaction-Modal");
+const session = localStorage.getItem("session");
+let logged = sessionStorage.getItem("logged");
+let data = {
+    transactions: []
+};
+
+//Logout da aplicacao
+document.getElementById("button-logout").addEventListener("click", logout);
+
+//Adicionar lancamentos
+document.getElementById("transaction-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checkd').value;
+
+    data.transactions.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveData(data);
+    e.target.reset();
+    myModal.hide();
+    alert("Lançamento adicionado com sucesso.")
+
+});
+
+checkLogged();
+
+function checkLogged() {
+    if (session) {
+        session.setItem("logged", session);
+        logged = session;
+    }
+    if (!logged) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const dataUser = localStorage.getItem(logged);
+    if (dataUser) {
+        data = JSON.parse(dataUser);
+    }
+    console.log(data);
+}
+
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+}
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
